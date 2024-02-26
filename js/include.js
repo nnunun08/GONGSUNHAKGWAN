15031059695ac9c36085a150b1b2b756a68b9507
evoke <nicodone@evokeui-MacBookAir.local>
2024-02-26 23:30:41 +0900

    create

---
+++ js/include.js
@@ -0,0 +1,20 @@
+// 퍼블리싱을 위한 include js
+// 컴포넌트 가져다 쓰기 위한 js로 서비스에는 불필요합니다
+
+
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
